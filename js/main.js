950166ff4eba58c29e2c83375f8c181af422a3a4
agustinelson <an.medinacolmenero@gmail.com>
2023-05-24 19:37:55 -0600

    Update index

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // ===== MENU SHOW =====
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-          nav = document.getElementById(navId)
-    
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
-    }      
+    }
 }
-showMenu('nav-toggle','nav-menu') 
+showMenu('nav-toggle', 'nav-menu')
 // ===== ACTIVE AND REMOVE MENU =====
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     //Active link
     navLink.forEach(n => n.classList.remove('active'))
     this.classList.add('active')
@@ -32,19 +32,39 @@ const sr = ScrollReveal({
 })
 // ===== SCROLL HOME =====
 sr.reveal('.home__title', {})
-sr.reveal('.button', {delay: 200})
-sr.reveal('.home__img', {delay: 400})
-sr.reveal('.home__social-icon', {interval: 200})
+sr.reveal('.button', { delay: 200 })
+sr.reveal('.home__img', { delay: 400 })
+sr.reveal('.home__social-icon', { interval: 200 })
 // ===== SCROLL ABOUT =====
 sr.reveal('.about__img', {})
-sr.reveal('.about__subtitle', {delay: 200})
-sr.reveal('.about__text', {delay: 400})
+sr.reveal('.about__subtitle', { delay: 200 })
+sr.reveal('.about__text', { delay: 400 })
 // ===== SCROLL SKILLS =====
+//==============
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__text', {delay: 200})
-sr.reveal('.skills__data', {interval: 200})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__text', { delay: 200 })
+sr.reveal('.skills__data', { interval: 200 })
+sr.reveal('.skills__img', { delay: 400 })
 // ===== SCROLL WORK =====
-sr.reveal('.work__img', {interval: 200})
+sr.reveal('.work__img', { interval: 200 })
 // ===== SCROLL CONTACT =====
-sr.reveal('.contact__input', {interval: 200})
+sr.reveal('.contact__input', { interval: 200 })
